c88f6f3eea4159d4adcffab7e6bae81976fea089
Varun Mishra <varundmishra@google.com>
2023-05-04 00:08:31 +0530

    Updated filenames/renamed objects

--- gateway.sh
+++ gateway.sh
@@ -17,23 +17,23 @@ kubectl apply -f manifests/backendconfig-default-backend.yaml
 kubectl apply -f manifests/nginx-default-backend.yaml
 
 #Deploy the demo applications
-kubectl apply -f manifests/store.yaml
-kubectl get pod -n store-en
-kubectl get svc -n store-en
-kubectl get pod -n store-de
-kubectl get svc -n store-de
-kubectl get pod -n store-canary
-kubectl get svc -n store-canary
+kubectl apply -f manifests/bookstore.yaml
+kubectl get pod -n bookstore-en
+kubectl get svc -n bookstore-en
+kubectl get pod -n bookstore-de
+kubectl get svc -n bookstore-de
+kubectl get pod -n bookstore-canary
+kubectl get svc -n bookstore-canary
 
 #Apply internal HTTP route
 kubectl apply -f manifests/route-internal-default-backend.yaml
 kubectl describe httproute route-internal-default-backend -n default
-kubectl apply -f manifests/route-internal-store-en.yaml
-kubectl describe httproute route-internal-store-en -n store-en
-kubectl apply -f manifests/route-internal-store-de.yaml
-kubectl describe httproute route-internal-store-de -n store-de
-kubectl apply -f manifests/route-internal-store-canary.yaml
-kubectl describe httproute route-internal-store-canary -n store-de
+kubectl apply -f manifests/route-internal-bookstore-en.yaml
+kubectl describe httproute route-internal-bookstore-en -n bookstore-en
+kubectl apply -f manifests/route-internal-bookstore-de.yaml
+kubectl describe httproute route-internal-bookstore-de -n bookstore-de
+kubectl apply -f manifests/route-internal-bookstore-canary.yaml
+kubectl describe httproute route-internal-bookstore-canary -n bookstore-de
 
 #Create a compute instance in the same vpc, test by sending traffic to your application from a VM
 #Create instance
@@ -41,11 +41,11 @@ gcloud compute instances create gateway-test-vm --machine-type e2-small --tags h
 #Get the virtual ip address of the internal gateway by running the following command
 kubectl get gateways.gateway.networking.k8s.io internal-http -o=jsonpath="{.status.addresses[0].value}"
 #SSH into the instance and test the application
-curl -vvv -H "host: store.example.com" http://INTERNAL_GATEWAY_IP
-curl -vvv -H "host: store.example.com" http://INTERNAL_GATEWAY_IP/en
-curl -vvv -H "host: store.example.com" http://INTERNAL_GATEWAY_IP/de
+curl -vvv -H "host: bookstore.example.com" http://INTERNAL_GATEWAY_IP
+curl -vvv -H "host: bookstore.example.com" http://INTERNAL_GATEWAY_IP/en
+curl -vvv -H "host: bookstore.example.com" http://INTERNAL_GATEWAY_IP/de
 #To test header based routing:
-curl -vvv -H "host: store.example.com" -H "env:canary" http://INTERNAL_GATEWAY_IP/canary
+curl -vvv -H "host: bookstore.example.com" -H "env:canary" http://INTERNAL_GATEWAY_IP/canary
 
 #Deploy the external gateway
 kubectl apply -f manifests/gateway-external.yaml
@@ -55,12 +55,12 @@ kubectl get gateways.gateway.networking.k8s.io external-http -o=jsonpath="{.stat
 #Apply external HTTP route
 kubectl apply -f manifests/route-external-default-backend.yaml
 kubectl describe httproute route-external-default-backend -n default
-kubectl apply -f manifests/route-external-store-en.yaml
-kubectl describe httproute route-external-store-en -n store-en
-kubectl apply -f manifests/route-external-store-de.yaml
-kubectl describe httproute route-external-store-de -n store-de
-kubectl apply -f manifests/route-external-store-canary.yaml
-kubectl describe httproute route-external-store-canary -n store-canary
+kubectl apply -f manifests/route-external-bookstore-en.yaml
+kubectl describe httproute route-external-bookstore-en -n bookstore-en
+kubectl apply -f manifests/route-external-bookstore-de.yaml
+kubectl describe httproute route-external-bookstore-de -n bookstore-de
+kubectl apply -f manifests/route-external-bookstore-canary.yaml
+kubectl describe httproute route-external-bookstore-canary -n bookstore-canary
 
 #Get the virtual ip address of the internal gateway by running the following command
 kubectl get gateways.gateway.networking.k8s.io external-http -o=jsonpath="{.status.addresses[0].value}"
